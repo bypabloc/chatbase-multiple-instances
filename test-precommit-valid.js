e92ecb1c3f8bcdfdfe90c4d00f5020b7181ed275
Pablo Contreras <pacg1991@gmail.com>
2025-06-27 00:08:02 -0500

    chore: remove test file

--- test-precommit-valid.js
+++
@@ -1,7 +0,0 @@
-// Test file that will be formatted by pre-commit
-const greeting =      "Hello World"
-const   number   =   42
-
-console.log(   greeting,    number   )
-
-export   {   greeting,    number   }
