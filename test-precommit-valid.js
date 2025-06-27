37c62effcf903919d604bcfe9d273044a4f1ff70
Pablo Contreras <pacg1991@gmail.com>
2025-06-27 00:07:28 -0500

    feat: add pre-commit hooks with Biome linting and formatting

---
+++ test-precommit-valid.js
@@ -0,0 +1,7 @@
+// Test file that will be formatted by pre-commit
+const greeting =      "Hello World"
+const   number   =   42
+
+console.log(   greeting,    number   )
+
+export   {   greeting,    number   }
